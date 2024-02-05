4f35bae67ab64c750ef991ce80ba86eb393b98e6
HenryToff <144433130+HenryToff@users.noreply.github.com>
2024-02-05 20:25:45 -0100

    Add files via upload

--- mail_handler.php
+++ mail_handler.php
@@ -1,21 +1,19 @@
 <?php
 if(isset($_POST['submit'])){
-    $name = $POST['name'];
-    $phone = $POST['phone'];
-    $email = $POST['email'];
-    $msg = $POST['msg'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $msg = $_POST['msg'];
 
     $to = 'henrydetoff@gmail.com';
     $subject = 'Form Submission';
-    $message = "Name: ".$name."\n"."Phone: ".$phone."\n". "wrote the following: "."\n\n.$msg;
-    $headers = Form: " .$email;
+    $message = "Name: ".$name."\n" ."wrote the following: "."\n\n".$msg;
+    $headers = "Form: " .$email;
 
     if (mail($to, $subject, $message, $headers)){
         echo "<h1>Message Sent! Thank you"." ".$name.", We will contact you shortly!</h1>";
 
         }
-        else "Something went wrong";
-}
-
-
+        else{ "Something went wrong";
+    }
+  }
 ?>
