6752ba98252c006c0c39802a9f3bca6a71653591
HenryToff <144433130+HenryToff@users.noreply.github.com>
2024-02-06 14:07:31 -0100

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,28 @@ menu.onclick = () => {
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
+
+
+
+var Username = document.getElementById('name').value;
+var email = document.getElementById('email').value;
+var msg = document.getElementById('msg').value;
+
+var messageBody = "Name" + Username +
+"<br/> Email" + email +
+"<br/> Message"+ msg;
+
+function emailSend(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "kyeiornament@gmail.com",
+        Password : "98E5C9D43BF205C649B6D0A9CE5CB1A0022F",
+        To : 'henrydetoff@gmail.com',
+        From : "kyeiornament@gmail.com",
+        Subject : "This is the subject",
+        Body : messageBody,
+    }).then(
+      message => alert(message)
+    );
+};
+
